424c2a40d055818a5030b7001072d55d38f94737
Fernando Guillen <fguillen.mail@gmail.com>
2021-12-18 20:50:12 +0100

    Only snowflakes

--- Assets/Scripts/SnowSpawnerController.cs
+++ Assets/Scripts/SnowSpawnerController.cs
@@ -7,12 +7,17 @@ public class SnowSpawnerController : MonoBehaviour
 {
     [SerializeField] GameObject snowflakePrefab;
     [SerializeField] float snowflakeEachSeconds = 1f;
+    [SerializeField] int initialSnowflakes = 100;
+    [SerializeField] float initialImpulseForce = 1f;
     [SerializeField] BoxCollider snowflakesZone;
     float nextSnowFlakeAt;
 
     void Start()
     {
-        SpawnSnowflake();
+        for (int i = 0; i < initialSnowflakes; i++)
+        {
+            SpawnSnowflake();
+        }
     }
 
     void Update()
@@ -24,7 +29,9 @@ public class SnowSpawnerController : MonoBehaviour
     void SpawnSnowflake()
     {
         Vector3 position = Utils.RandomPointInCollider(snowflakesZone);
-        Instantiate(snowflakePrefab, position, Quaternion.identity, transform);
+        GameObject snowflake = Instantiate(snowflakePrefab, position, Quaternion.identity, transform);
+        float force = Utils.AddNoise(initialImpulseForce);
+        snowflake.GetComponent<Rigidbody>().AddForce(Vector3.down * force, ForceMode.Impulse);
         nextSnowFlakeAt = Time.time + Utils.AddNoise(snowflakeEachSeconds);
     }
 }
